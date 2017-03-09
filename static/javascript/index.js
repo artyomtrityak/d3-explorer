1dc16ade74afe462ddced01e71070cacf772c6ca
Artyom Trityak <art.trityak@gmail.com>
2017-03-08 20:28:33 -0800

    remove map

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -16,10 +16,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById(('root'))
 );
-
-
-import L from 'leaflet';
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mymap);
