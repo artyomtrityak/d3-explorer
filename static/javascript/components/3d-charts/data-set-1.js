7b80a985bc7a40bafc12014f9511536fdfc6eb77
Artyom Trityak <art.trityak@gmail.com>
2018-03-14 14:15:30 -0700

    3D D3 params dataset

---
+++ static/javascript/components/3d-charts/data-set-1.js
@@ -0,0 +1,716 @@
+module.exports = {
+  nodes: [
+    {
+      id: "App1",
+      type: "application",
+      val: 30
+    },
+    {
+      id: "Service1",
+      type: "microservice",
+      val: 10
+    },
+    {
+      id: "Service2",
+      type: "microservice",
+      val: 10
+    },
+    {
+      id: "Service3",
+      type: "microservice",
+      val: 10
+    },
+    {
+      id: "Service11",
+      type: "microservice",
+      val: 10
+    },
+    {
+      id: "Service21",
+      type: "microservice",
+      val: 10
+    },
+
+    {
+      id: "DEV-ENV",
+      type: "environment",
+      val: 10
+    },
+    {
+      id: "DEV-K8Cluster",
+      type: "K8Cluster"
+    },
+    {
+      id: "DEV-K8Service1",
+      type: "K8Service"
+    },
+    {
+      id: "DEV-K8Service1-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service1-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service1-Pod3",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service1-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service1-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service1-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service1-Pod2-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service1-Pod3-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service1-Pod3-Container2",
+      type: "K8Container"
+    },
+
+    {
+      id: "DEV-K8Service2",
+      type: "K8Service"
+    },
+    {
+      id: "DEV-K8Service2-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service2-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service2-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service2-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service2-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service2-Pod2-Container2",
+      type: "K8Container"
+    },
+
+    {
+      id: "DEV-K8Service3",
+      type: "K8Service"
+    },
+    {
+      id: "DEV-K8Service3-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service3-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service3-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service3-Pod2-Container1",
+      type: "K8Container"
+    },
+
+    {
+      id: "DEV-K8Service11",
+      type: "K8Service"
+    },
+    {
+      id: "DEV-K8Service11-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service11-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service11-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service11-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service11-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service11-Pod2-Container2",
+      type: "K8Container"
+    },
+
+    {
+      id: "DEV-K8Service21",
+      type: "K8Service"
+    },
+    {
+      id: "DEV-K8Service21-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service21-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "DEV-K8Service21-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service21-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service21-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "DEV-K8Service21-Pod2-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-ENV",
+      type: "environment",
+      val: 10
+    },
+    {
+      id: "PROD-K8Cluster",
+      type: "K8Cluster"
+    },
+    {
+      id: "PROD-K8Service1",
+      type: "K8Service"
+    },
+    {
+      id: "PROD-K8Service1-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service1-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service1-Pod3",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service1-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service1-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service1-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service1-Pod2-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service1-Pod3-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service1-Pod3-Container2",
+      type: "K8Container"
+    },
+
+    {
+      id: "PROD-K8Service2",
+      type: "K8Service"
+    },
+    {
+      id: "PROD-K8Service2-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service2-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service2-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service2-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service2-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service2-Pod2-Container2",
+      type: "K8Container"
+    },
+
+    {
+      id: "PROD-K8Service3",
+      type: "K8Service"
+    },
+    {
+      id: "PROD-K8Service3-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service3-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service3-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service3-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service11",
+      type: "K8Service"
+    },
+    {
+      id: "PROD-K8Service11-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service11-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service11-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service11-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service11-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service11-Pod2-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service21",
+      type: "K8Service"
+    },
+    {
+      id: "PROD-K8Service21-Pod1",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service21-Pod2",
+      type: "K8Pod"
+    },
+    {
+      id: "PROD-K8Service21-Pod1-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service21-Pod1-Container2",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service21-Pod2-Container1",
+      type: "K8Container"
+    },
+    {
+      id: "PROD-K8Service21-Pod2-Container2",
+      type: "K8Container"
+    }
+  ],
+  links: [
+    {
+      source: "App1",
+      target: "Service1"
+    },
+    {
+      source: "App1",
+      target: "Service2"
+    },
+    {
+      source: "App1",
+      target: "Service3"
+    },
+    {
+      source: "App1",
+      target: "DEV-ENV"
+    },
+    {
+      source: "App1",
+      target: "PROD-ENV"
+    },
+    {
+      source: "Service1",
+      target: "Service11"
+    },
+    {
+      source: "Service2",
+      target: "Service21"
+    },
+    {
+      source: "DEV-ENV",
+      target: "DEV-K8Cluster"
+    },
+    {
+      source: "Service1",
+      target: "DEV-K8Service1"
+    },
+    {
+      source: "Service2",
+      target: "DEV-K8Service2"
+    },
+    {
+      source: "Service3",
+      target: "DEV-K8Service3"
+    },
+    {
+      source: "Service11",
+      target: "DEV-K8Service11"
+    },
+    {
+      source: "Service21",
+      target: "DEV-K8Service21"
+    },
+    {
+      source: "DEV-K8Cluster",
+      target: "DEV-K8Service1"
+    },
+    {
+      source: "DEV-K8Cluster",
+      target: "DEV-K8Service2"
+    },
+    {
+      source: "DEV-K8Cluster",
+      target: "DEV-K8Service3"
+    },
+    {
+      source: "DEV-K8Cluster",
+      target: "DEV-K8Service11"
+    },
+    {
+      source: "DEV-K8Cluster",
+      target: "DEV-K8Service21"
+    },
+    {
+      source: "DEV-K8Service1",
+      target: "DEV-K8Service1-Pod1"
+    },
+    {
+      source: "DEV-K8Service1",
+      target: "DEV-K8Service1-Pod2"
+    },
+    {
+      source: "DEV-K8Service1",
+      target: "DEV-K8Service1-Pod3"
+    },
+    {
+      source: "DEV-K8Service1-Pod1",
+      target: "DEV-K8Service1-Pod1-Container1"
+    },
+    {
+      source: "DEV-K8Service1-Pod1",
+      target: "DEV-K8Service1-Pod1-Container2"
+    },
+    {
+      source: "DEV-K8Service1-Pod2",
+      target: "DEV-K8Service1-Pod2-Container1"
+    },
+    {
+      source: "DEV-K8Service1-Pod2",
+      target: "DEV-K8Service1-Pod2-Container2"
+    },
+    {
+      source: "DEV-K8Service1-Pod3",
+      target: "DEV-K8Service1-Pod3-Container1"
+    },
+    {
+      source: "DEV-K8Service1-Pod3",
+      target: "DEV-K8Service1-Pod3-Container2"
+    },
+    {
+      source: "DEV-K8Service2",
+      target: "DEV-K8Service2-Pod1"
+    },
+    {
+      source: "DEV-K8Service2",
+      target: "DEV-K8Service2-Pod2"
+    },
+    {
+      source: "DEV-K8Service2-Pod1",
+      target: "DEV-K8Service2-Pod1-Container1"
+    },
+    {
+      source: "DEV-K8Service2-Pod1",
+      target: "DEV-K8Service2-Pod1-Container2"
+    },
+    {
+      source: "DEV-K8Service2-Pod2",
+      target: "DEV-K8Service2-Pod2-Container1"
+    },
+    {
+      source: "DEV-K8Service2-Pod2",
+      target: "DEV-K8Service2-Pod2-Container2"
+    },
+    {
+      source: "DEV-K8Service3",
+      target: "DEV-K8Service3-Pod1"
+    },
+    {
+      source: "DEV-K8Service3",
+      target: "DEV-K8Service3-Pod2"
+    },
+    {
+      source: "DEV-K8Service3-Pod1",
+      target: "DEV-K8Service3-Pod1-Container1"
+    },
+    {
+      source: "DEV-K8Service3-Pod2",
+      target: "DEV-K8Service3-Pod2-Container1"
+    },
+    {
+      source: "DEV-K8Service11",
+      target: "DEV-K8Service11-Pod1"
+    },
+    {
+      source: "DEV-K8Service11",
+      target: "DEV-K8Service11-Pod2"
+    },
+    {
+      source: "DEV-K8Service11-Pod1",
+      target: "DEV-K8Service11-Pod1-Container1"
+    },
+    {
+      source: "DEV-K8Service11-Pod1",
+      target: "DEV-K8Service11-Pod1-Container2"
+    },
+    {
+      source: "DEV-K8Service11-Pod2",
+      target: "DEV-K8Service11-Pod2-Container1"
+    },
+    {
+      source: "DEV-K8Service11-Pod2",
+      target: "DEV-K8Service11-Pod2-Container2"
+    },
+    {
+      source: "DEV-K8Service21",
+      target: "DEV-K8Service21-Pod1"
+    },
+    {
+      source: "DEV-K8Service21",
+      target: "DEV-K8Service21-Pod2"
+    },
+    {
+      source: "DEV-K8Service21-Pod1",
+      target: "DEV-K8Service21-Pod1-Container1"
+    },
+    {
+      source: "DEV-K8Service21-Pod1",
+      target: "DEV-K8Service21-Pod1-Container2"
+    },
+    {
+      source: "DEV-K8Service21-Pod2",
+      target: "DEV-K8Service21-Pod2-Container1"
+    },
+    {
+      source: "DEV-K8Service21-Pod2",
+      target: "DEV-K8Service21-Pod2-Container2"
+    },
+    {
+      source: "PROD-ENV",
+      target: "PROD-K8Cluster"
+    },
+    {
+      source: "Service1",
+      target: "DEV-K8Service1"
+    },
+    {
+      source: "Service2",
+      target: "PROD-K8Service2"
+    },
+    {
+      source: "Service3",
+      target: "PROD-K8Service3"
+    },
+    {
+      source: "Service11",
+      target: "PROD-K8Service11"
+    },
+    {
+      source: "Service21",
+      target: "PROD-K8Service21"
+    },
+    {
+      source: "PROD-K8Cluster",
+      target: "PROD-K8Service1"
+    },
+    {
+      source: "PROD-K8Cluster",
+      target: "PROD-K8Service2"
+    },
+    {
+      source: "PROD-K8Cluster",
+      target: "PROD-K8Service3"
+    },
+    {
+      source: "PROD-K8Cluster",
+      target: "PROD-K8Service11"
+    },
+    {
+      source: "PROD-K8Cluster",
+      target: "PROD-K8Service21"
+    },
+    {
+      source: "PROD-K8Service1",
+      target: "PROD-K8Service1-Pod1"
+    },
+    {
+      source: "PROD-K8Service1",
+      target: "PROD-K8Service1-Pod2"
+    },
+    {
+      source: "PROD-K8Service1",
+      target: "PROD-K8Service1-Pod3"
+    },
+    {
+      source: "PROD-K8Service1-Pod1",
+      target: "PROD-K8Service1-Pod1-Container1"
+    },
+    {
+      source: "PROD-K8Service1-Pod1",
+      target: "PROD-K8Service1-Pod1-Container2"
+    },
+    {
+      source: "PROD-K8Service1-Pod2",
+      target: "PROD-K8Service1-Pod2-Container1"
+    },
+    {
+      source: "PROD-K8Service1-Pod2",
+      target: "PROD-K8Service1-Pod2-Container2"
+    },
+    {
+      source: "PROD-K8Service1-Pod3",
+      target: "PROD-K8Service1-Pod3-Container1"
+    },
+    {
+      source: "PROD-K8Service1-Pod3",
+      target: "PROD-K8Service1-Pod3-Container2"
+    },
+    {
+      source: "PROD-K8Service2",
+      target: "PROD-K8Service2-Pod1"
+    },
+    {
+      source: "PROD-K8Service2",
+      target: "PROD-K8Service2-Pod2"
+    },
+    {
+      source: "PROD-K8Service2-Pod1",
+      target: "PROD-K8Service2-Pod1-Container1"
+    },
+    {
+      source: "PROD-K8Service2-Pod1",
+      target: "PROD-K8Service2-Pod1-Container2"
+    },
+    {
+      source: "PROD-K8Service2-Pod2",
+      target: "PROD-K8Service2-Pod2-Container1"
+    },
+    {
+      source: "PROD-K8Service2-Pod2",
+      target: "PROD-K8Service2-Pod2-Container2"
+    },
+
+    {
+      source: "PROD-K8Service3",
+      target: "PROD-K8Service3-Pod1"
+    },
+    {
+      source: "PROD-K8Service3",
+      target: "PROD-K8Service3-Pod2"
+    },
+    {
+      source: "PROD-K8Service3-Pod1",
+      target: "PROD-K8Service3-Pod1-Container1"
+    },
+    {
+      source: "PROD-K8Service3-Pod2",
+      target: "PROD-K8Service3-Pod2-Container1"
+    },
+
+    {
+      source: "PROD-K8Service11",
+      target: "PROD-K8Service11-Pod1"
+    },
+    {
+      source: "PROD-K8Service11",
+      target: "PROD-K8Service11-Pod2"
+    },
+    {
+      source: "PROD-K8Service11-Pod1",
+      target: "PROD-K8Service11-Pod1-Container1"
+    },
+    {
+      source: "PROD-K8Service11-Pod1",
+      target: "PROD-K8Service11-Pod1-Container2"
+    },
+    {
+      source: "PROD-K8Service11-Pod2",
+      target: "PROD-K8Service11-Pod2-Container1"
+    },
+    {
+      source: "PROD-K8Service11-Pod2",
+      target: "PROD-K8Service11-Pod2-Container2"
+    },
+
+    {
+      source: "PROD-K8Service21",
+      target: "PROD-K8Service21-Pod1"
+    },
+    {
+      source: "PROD-K8Service21",
+      target: "PROD-K8Service21-Pod2"
+    },
+    {
+      source: "PROD-K8Service21-Pod1",
+      target: "PROD-K8Service21-Pod1-Container1"
+    },
+    {
+      source: "PROD-K8Service21-Pod1",
+      target: "PROD-K8Service21-Pod1-Container2"
+    },
+    {
+      source: "PROD-K8Service21-Pod2",
+      target: "PROD-K8Service21-Pod2-Container1"
+    },
+    {
+      source: "PROD-K8Service21-Pod2",
+      target: "PROD-K8Service21-Pod2-Container2"
+    }
+  ]
+};
--- static/javascript/components/3d-charts/force-3d-d3.js
+++ static/javascript/components/3d-charts/force-3d-d3.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import * as d3 from "d3";
 import ForceGraph3D from "3d-force-graph";
 import * as THREE from "three";
+import DATASET_1 from "./data-set-1";
 
 export default class TreeDBasicChart extends React.Component {
   constructor(props) {
@@ -11,17 +12,7 @@ export default class TreeDBasicChart extends React.Component {
     const GROUPS = 5;
 
     this.state = {
-      data: {
-        nodes: [...Array(N).keys()].map(i => ({
-          id: i,
-          group: Math.ceil(Math.random() * GROUPS),
-          val: Math.ceil(Math.random() * GROUPS)
-        })),
-        links: [...Array(N).keys()].filter(id => id).map(id => ({
-          source: id,
-          target: Math.round(Math.random() * (id - 1))
-        }))
-      }
+      data: DATASET_1
     };
   }
 
@@ -30,35 +21,63 @@ export default class TreeDBasicChart extends React.Component {
       .width(this.chartRef.clientWidth - 100)
       .height(window.innerHeight - 200)
       .backgroundColor("#FFFFFF")
-      .nodeAutoColorBy("group")
+      .nodeColor(d => {
+        switch (d.type) {
+          case "application":
+            return "blue";
+          case "environment":
+            return "yellow";
+          case "microservice":
+            return "purple";
+          case "K8Container":
+            return "green";
+          case "K8Pod":
+            return "red";
+          case "K8Cluster":
+            return "black";
+          default:
+            return "gray";
+        }
+      })
       .linkWidth(3)
-      .linkColor((d, d2) => {
-        return d.source < 20 ? "#FF0000" : "#000000";
+      .linkColor((d) => {
+        return d.type === "application" ? "#FF0000" : "#000000";
       })
       .graphData(this.state.data);
 
-    //this.graph.d3Force('charge').strength(-200);
-    // this.graph.d3Force('link').distance(function(d) {
-    //   if (d.index < 20) {
-    //     return 90;
-    //   }
-    //   return 50;
-    // });
-
-    this.graph.d3Force('link').strength((d) => {
-      if (d.index < 20) {
-        return 0.5;
+    this.graph.d3Force("charge").strength(-250);
+    this.graph.d3Force("charge").distanceMin(150);
+    this.graph.d3Force(
+      "r",
+      d3.forceRadial(function(d) {
+        if (d.type === "application" || d.type === "microservice") {
+          return 20;
+        } else {
+          return 100;
+        }
+      })
+    );
+    this.graph.d3Force("link").distance(d => {
+      if (d.type === "application" || d.type === "microservice") {
+        return 120;
       }
-      return 2;
+      return 20;
     });
 
-    this.graph.d3Force('charge').strength(-150);
-    //this.graph.d3Force('charge').distanceMin(100);
-    //this.graph.d3Force('radial', d3.forceRadial(150));
-    // this.graph.d3Force("r", d3.forceRadial(function(d) {
-    //   return d.index < 10 ? 100 : 300;
-    // }));
-    // this.graph.d3Force('link').distance(50).strength(1);
+    this.startOrbit();
+  }
+
+  startOrbit() {
+    const distance = 800;
+    let angle = 0;
+
+    this.interval = setInterval(() => {
+      angle = angle + Math.PI / 300;
+      this.graph.cameraPosition({
+        x: distance * Math.sin(angle),
+        z: distance * Math.cos(angle)
+      });
+    }, 30);
   }
 
   render() {
