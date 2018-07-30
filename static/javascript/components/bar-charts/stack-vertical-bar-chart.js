6036db88ded184dfb0988d9d70e0932bc6ffc797
Artyom Trityak <art.trityak@gmail.com>
2018-07-30 12:50:23 -0700

    Stack vertical part 1

--- static/javascript/components/bar-charts/stack-vertical-bar-chart.js
+++ static/javascript/components/bar-charts/stack-vertical-bar-chart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as d3 from "d3";
-import { generateArray } from "../../data-layer/array-processors";
+import WithSize from "../../shared/with-size";
 
 function stackMin(serie) {
   return d3.min(serie, d => d[0]);
@@ -10,39 +10,41 @@ function stackMax(serie) {
   return d3.max(serie, d => d[1]);
 }
 
-export default class SvgVerticalBarChart extends React.Component {
+const MARGINS = { top: 20, right: 30, bottom: 30, left: 40 };
+
+class StackVerticalBarChart extends React.Component {
+  static displayName = "StackVerticalBarChart";
+
   constructor(props) {
     super(props);
     this.state = {
+      keys: ["apples", "bananas", "cherries"],
       data: [
-        { month: "Q1-2017", apples: 3840, bananas: 1920, cherries: 1960 },
-        { month: "Q2-2017", apples: 1600, bananas: 1440, cherries: 960 },
-        { month: "Q3-2017", apples: 640, bananas: 960, cherries: 640 },
-        { month: "Q4-2017", apples: 320, bananas: 480, cherries: 640 }
+        { month: "Q1-2017", apples: 300, bananas: 500, cherries: 700, total: 1500 },
+        { month: "Q2-2017", apples: 500, bananas: 400, cherries: 1000, total: 1900 },
+        { month: "Q3-2017", apples: 700, bananas: 200, cherries: 500, total: 1400 },
+        { month: "Q4-2017", apples: 300, bananas: 700, cherries: 200, total: 1200 },
+        { month: "Q1-2018", apples: 300, bananas: 700, cherries: 200, total: 1200 },
+        { month: "Q2-2018", apples: 500, bananas: 400, cherries: 1000, total: 1900 },
+        { month: "Q3-2018", apples: 300, bananas: 500, cherries: 700, total: 1500 }
       ]
     };
   }
 
   componentDidMount() {
-    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    const width = 500 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-
     const chart = d3
       .select(this.chartRef)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
+      .attr("width", this.props.width)
+      .attr("height", this.props.height);
 
     this.chartInner = chart
       .append("g")
       .attr("class", "chart-inner")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+      .attr("transform", `translate(${MARGINS.left},${MARGINS.top})`);
 
-    const series = d3.stack().keys(["apples", "bananas", "cherries"])(
-      this.state.data
-    );
+    const series = d3.stack().keys(this.state.keys)(this.state.data);
 
-    const { xScale, yScale } = this.getScales(width, height, margin, series);
+    const { xScale, yScale } = this.getScales(this.props.width, this.props.height, series);
 
     this.chartInner
       .append("g")
@@ -51,7 +53,7 @@ export default class SvgVerticalBarChart extends React.Component {
 
     this.chartInner
       .append("g")
-      .attr("transform", "translate(" + margin.left + ",0)")
+      .attr("transform", "translate(" + MARGINS.left + ",0)")
       .call(d3.axisLeft(yScale));
 
     const colors = d3.scaleOrdinal(d3.schemeCategory10);
@@ -71,35 +73,36 @@ export default class SvgVerticalBarChart extends React.Component {
       .attr("x", d => xScale(d.data.month))
       .attr("y", d => yScale(d[1]))
       .attr("height", d => yScale(d[0]) - yScale(d[1]));
-
-    this.processing();
   }
 
-  processing() {}
-
-  getScales(width, height, margin, series) {
+  getScales(width, height, series) {
     const xScale = d3
       .scaleBand()
       .domain(this.state.data.map(d => d.month))
-      .rangeRound([margin.left, width - margin.right])
+      .rangeRound([MARGINS.left, width - MARGINS.right])
       .padding(0.1);
 
     const yScale = d3
       .scaleLinear()
       .domain([d3.min(series, stackMin), d3.max(series, stackMax)])
-      .rangeRound([height - margin.bottom, margin.top]);
+      .rangeRound([height - MARGINS.bottom, MARGINS.top]);
 
     return { xScale, yScale };
   }
 
+  createScaleX() {}
+
+  createScaleY() {}
+
   render() {
     return (
       <div>
-        <svg
-          className="bar-chart bar-chart--stack"
-          ref={r => (this.chartRef = r)}
-        />
+        <svg className="bar-chart bar-chart--stack" ref={r => (this.chartRef = r)} />
       </div>
     );
   }
+
+  renderStack(stack) {}
 }
+
+export default WithSize(StackVerticalBarChart);
